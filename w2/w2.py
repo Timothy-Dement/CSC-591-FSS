cf73d562bcaf92f5e88522a8aede96c54d56efa3
Timothy-Dement <tmdement@ncsu.edu>
2018-09-05 15:41:47 -0400

    Update w2.py and w2.txt with second-pass implementations

--- w2/w2.py
+++ w2/w2.py
@@ -67,14 +67,21 @@ DATA2 = """
     rainy,71,91,TRUE,no"""
 
 
-def lines(s):
+########
+#
+#  Functions beginning with my_ were my first attempts before Dr. Menzies posted his solutions.
+#  Functions not beginning with my_ added in logic from the posted solutions.
+#
+########
+
+def my_lines(s):
     """"
     Return contents, one line at a time.
     """
     return s.splitlines()
 
 
-def rows(src):
+def my_rows(src):
     """
     Kill bad characters. If line ends in ',' then join to next. Skip blank lines.
     """
@@ -91,7 +98,7 @@ def rows(src):
     return stripped_rows
 
 
-def cols(src):
+def my_cols(src):
     """
     If a column name on row1 contains '?' then skip over that column.
     """
@@ -115,7 +122,7 @@ def cols(src):
     return split_rows
 
 
-def prep(src):
+def my_prep(src):
     """
     If a column name on row1 contains '$' coerce strings in that column to a float.
     """
@@ -137,33 +144,193 @@ def prep(src):
 
 def ok0(s):
     correct = [['outlook', '$temp', 'windy', 'play'],
-             ['sunny', 85.0, 'FALSE', 'no'],
-             ['sunny', 80.0, 'TRUE', 'no'],
-             ['overcast', 83.0, 'FALSE', 'yes'],
-             ['rainy', 70.0, 'FALSE', 'yes'],
-             ['rainy', 68.0, 'FALSE', 'yes'],
-             ['rainy', 65.0, 'TRUE', 'no'],
-             ['overcast', 64.0, 'TRUE', 'yes'],
-             ['sunny', 72.0, 'FALSE', 'no'],
-             ['sunny', 69.0, 'FALSE', 'yes'],
-             ['rainy', 75.0, 'FALSE', 'yes'],
-             ['sunny', 75.0, 'TRUE', 'yes'],
-             ['overcast', 100.0, 'TRUE', 'yes'],
-             ['overcast', 81.0, 'FALSE', 'yes'],
-             ['rainy', 71.0, 'TRUE', 'no']]
+               ['sunny', 85.0, 'FALSE', 'no'],
+               ['sunny', 80.0, 'TRUE', 'no'],
+               ['overcast', 83.0, 'FALSE', 'yes'],
+               ['rainy', 70.0, 'FALSE', 'yes'],
+               ['rainy', 68.0, 'FALSE', 'yes'],
+               ['rainy', 65.0, 'TRUE', 'no'],
+               ['overcast', 64.0, 'TRUE', 'yes'],
+               ['sunny', 72.0, 'FALSE', 'no'],
+               ['sunny', 69.0, 'FALSE', 'yes'],
+               ['rainy', 75.0, 'FALSE', 'yes'],
+               ['sunny', 75.0, 'TRUE', 'yes'],
+               ['overcast', 100.0, 'TRUE', 'yes'],
+               ['overcast', 81.0, 'FALSE', 'yes'],
+               ['rainy', 71.0, 'TRUE', 'no']]
+
+    for index, row in enumerate(my_prep(my_cols(my_rows(my_lines(s))))):
+        print(row)
+        assert row == correct[index]
+
+
+def lines(s):
+    """"
+    Return contents, one line at a time.
+    """
+
+    for line in s.splitlines(): yield line
+
+
+def rows(src):
+    """
+    Kill bad characters. If line ends in ',' then join to next. Skip blank lines.
+    """
+
+    cache = []  # Will be used to collect 'partial' lines
+
+    for line in src:
+
+        line = re.sub(r'[ \n\r\t]|#.*', '', line)  # Remove 'blank' chars and all '#' and anything after
+
+        if line:  # Only consider non-blank lines
+
+            cache += [line]  # Add the line to the end of the cache
+
+            if not line.endswith(','):  # If the line does not end with ',' ...
+
+                line = ''.join(cache)  # ... then we are returning the cache contents
+
+                cache = []  # ... reset the cache
+
+                yield line  # ... and yield the line
+
+
+def cols(src):
+    """
+    If a column name on row1 contains '?' then skip over that column.
+    """
+
+    invalid_cols = set()
+
+    headers = []
+
+    for index, row in enumerate(src):
+
+        if index == 0:
+
+            headers = row.split(',')
+
+            for index, col in enumerate(headers):
+
+                if '?' in col: invalid_cols.add(index)
+
+        row_builder = []
+
+        row = row.split(',')
+
+        for index, col in enumerate(row):
+
+            if index not in invalid_cols: row_builder.append(col)
+
+        yield row_builder
+
+
+def prep(src):
+    """
+    If a column name on row1 contains '$' coerce strings in that column to a float.
+    """
+
+    num_cols = set()
+
+    for row_index, row in enumerate(src):
+
+        if row_index == 0:
+
+            for col_index, col in enumerate(row):
+
+                if '$' in col: num_cols.add(col_index)
+
+            yield row
+
+        else:
+
+            for num_col in num_cols:
+
+                row[num_col] = float(row[num_col])
+
+            yield row
+
+
+def ok0(s):
+    correct = [['outlook', '$temp', 'windy', 'play'],
+               ['sunny', 85.0, 'FALSE', 'no'],
+               ['sunny', 80.0, 'TRUE', 'no'],
+               ['overcast', 83.0, 'FALSE', 'yes'],
+               ['rainy', 70.0, 'FALSE', 'yes'],
+               ['rainy', 68.0, 'FALSE', 'yes'],
+               ['rainy', 65.0, 'TRUE', 'no'],
+               ['overcast', 64.0, 'TRUE', 'yes'],
+               ['sunny', 72.0, 'FALSE', 'no'],
+               ['sunny', 69.0, 'FALSE', 'yes'],
+               ['rainy', 75.0, 'FALSE', 'yes'],
+               ['sunny', 75.0, 'TRUE', 'yes'],
+               ['overcast', 100.0, 'TRUE', 'yes'],
+               ['overcast', 81.0, 'FALSE', 'yes'],
+               ['rainy', 71.0, 'TRUE', 'no']]
+
+    print()
+
+    for index, row in enumerate(my_prep(my_cols(my_rows(my_lines(s))))):
+        print(row)
+        assert row == correct[index]
+
+    print()
+
+@O.k
+def ok1():
+    """
+    Testing original implementation on standard data.
+    """
+    ok0(DATA1)
+
+
+@O.k
+def ok2():
+    """
+    Testing original implementation on nonstandard data.
+    """
+    ok0(DATA2)
+
+
+def ok3(s):
+    correct = [['outlook', '$temp', 'windy', 'play'],
+               ['sunny', 85.0, 'FALSE', 'no'],
+               ['sunny', 80.0, 'TRUE', 'no'],
+               ['overcast', 83.0, 'FALSE', 'yes'],
+               ['rainy', 70.0, 'FALSE', 'yes'],
+               ['rainy', 68.0, 'FALSE', 'yes'],
+               ['rainy', 65.0, 'TRUE', 'no'],
+               ['overcast', 64.0, 'TRUE', 'yes'],
+               ['sunny', 72.0, 'FALSE', 'no'],
+               ['sunny', 69.0, 'FALSE', 'yes'],
+               ['rainy', 75.0, 'FALSE', 'yes'],
+               ['sunny', 75.0, 'TRUE', 'yes'],
+               ['overcast', 100.0, 'TRUE', 'yes'],
+               ['overcast', 81.0, 'FALSE', 'yes'],
+               ['rainy', 71.0, 'TRUE', 'no']]
+
+    print()
 
     for index, row in enumerate(prep(cols(rows(lines(s))))):
         print(row)
         assert row == correct[index]
 
+    print()
 
 @O.k
-def ok1(): ok0(DATA1)
-
+def ok4():
+    """
+    Testing second implementation on standard data.
+    """
+    ok3(DATA1)
 
 @O.k
-def ok2(): ok0(DATA2)
-
+def ok5():
+    """
+    Testing second implementationon nonstandard data.
+    """
+    ok3(DATA2)
 
 if __name__ == "__main__":
     O.report()
