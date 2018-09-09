ba764b6cacb8929675fe26548bc1a1d123ef5394
Timothy-Dement <tmdement@ncsu.edu>
2018-09-09 18:01:25 -0400

    Complete num.py, sym.py and associated tests

---
+++ w3/sym.py
@@ -0,0 +1,59 @@
+from math import log2
+
+class Sym:
+
+    def __init__(self):
+
+        self.sym_dict = {}
+        self.mode = None
+        self.count = 0
+        self.entropy = None
+
+    def sym_inc(self, new):
+
+        self.entropy = None
+
+        self.count += 1
+
+        if new in self.sym_dict:
+            self.sym_dict[new] += 1
+
+        else:
+            self.sym_dict[new] = 1
+
+        if not self.mode:
+            self.mode = new
+
+        elif self.sym_dict[self.mode] < self.sym_dict[new]:
+            self.mode = new
+
+    def syms(self, sym_list):
+
+        for sym in sym_list:
+            self.sym_inc(sym)
+
+    def sym_dec(self, remove):
+
+        self.entropy = None
+
+        self.count -= 1
+
+        if self.sym_dict[remove] == 1:
+            del self.sym_dict[remove]
+
+        else:
+            self.sym_dict[remove] -= 1
+
+    def sym_ent(self):
+
+        if not self.entropy:
+
+            self.entropy = 0
+
+            for sym in self.sym_dict:
+
+                probability = self.sym_dict[sym] / self.count
+
+                self.entropy -= probability * log2(probability)
+
+        return self.entropy
--- w3/w3.py
+++ w3/w3.py
@@ -1,6 +1,9 @@
 import math, random, re, traceback
 
 from sample import Sample
+from num import Num
+from sym import Sym
+
 
 class O:
     y = n = 0
@@ -26,37 +29,84 @@ class O:
 
 
 def close(x, y, c=0.01):
-    return math.fabs((x - y) / x) < c
+    return math.fabs((x - y) / y) < c
+
 
 @O.k
 def sample_test():
     """
-    Testing sample.py
+    Testing sample.py ...
     """
 
     samples = []
 
     for i in range(5, 11):
+        samples.append(Sample(2 ** i))
 
-        samples.append(Sample(2**i))
-
-    for i in range(10000):
+    for i in range(10 ** 4):
 
         random_number = random.random()
 
         for sample in samples:
-
             sample.sample_inc(random_number)
 
     print()
 
     for sample in samples:
-
-        print(sample.maximum_size, sample.nth(0.5))
+        print(sample.maximum_size, ':', sample.nth(0.5))
 
         assert close(sample.nth(0.5), 0.5, 0.2)
 
     print()
 
-if __name__== "__main__":
-  O.report()
+
+@O.k
+def num_test():
+    """
+    Testing num.py ...
+    """
+
+    num = Num(30)
+
+    num_list = [4, 10, 15, 38, 54,
+                57, 62, 83, 100, 100,
+                174, 190, 215, 225, 233,
+                250, 260, 270, 299, 300,
+                306, 333, 350, 375, 443,
+                475, 525, 583, 780, 1000]
+
+    num.nums(num_list)
+
+    print()
+
+    print('MU', ':', num.mean)
+    print('SD', ':', num.standard_deviation)
+
+    assert close(num.mean, 270.3)
+    assert close(num.standard_deviation, 231.946)
+
+    print()
+
+
+@O.k
+def sym_test():
+    """
+    Testing sym.py ...
+    """
+
+    sym = Sym()
+
+    sym_list = ['y', 'y', 'y', 'y', 'y', 'y', 'y',
+                'y', 'y', 'n', 'n', 'n', 'n', 'n']
+
+    sym.syms(sym_list)
+
+    print()
+    print('ENT', ':', sym.sym_ent())
+    print()
+
+    assert(close(sym.sym_ent(), 0.9403))
+
+
+if __name__ == "__main__":
+    O.report()
